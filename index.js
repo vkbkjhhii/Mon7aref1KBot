57795f2aea112a58f51a569031aa758cc1a5f8a6
vkbkjhhii <khas66258@gmail.com>
2026-02-25 03:20:23 +0200

    Add commands and responses for bot functionality

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const { Telegraf } = require("telegraf");
 
-if (!process.env.BOT_TOKEN) {
-  console.error("BOT_TOKEN is missing!");
-  process.exit(1);
-}
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply("البوت اشتغل 🔥"));
+// أمر start
+bot.start((ctx) => {
+  ctx.reply("أهلاً بيك في بوت المبرمج فرعون 👑\nاكتب /help عشان تشوف الأوامر");
+});
+
+// أمر help
+bot.command("help", (ctx) => {
+  ctx.reply(`
+الأوامر المتاحة:
+
+/start - تشغيل البوت
+/help - عرض الأوامر
+/id - معرفة الايدي بتاعك
+  `);
+});
+
+// أمر id
+bot.command("id", (ctx) => {
+  ctx.reply(`الايدي بتاعك هو: ${ctx.from.id}`);
+});
+
+// أي رسالة عادية
+bot.on("text", (ctx) => {
+  ctx.reply("مش فاهمك 🤖 جرب /help");
+});
+
 bot.launch();
 
 console.log("Bot is running...");
