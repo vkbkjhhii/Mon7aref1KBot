667180466ed750c78ee159e0cd82c9e920795fb5
vkbkjhhii <khas66258@gmail.com>
2026-02-25 02:26:29 +0200

    Migrate from node-telegram-bot-api to Telegraf

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const TelegramBot = require('node-telegram-bot-api');
+const { Telegraf } = require("telegraf");
 
-const token = "8768225608:AAHkYYcW-3wIUykW9UIGMh1qug8XtxIPuwY";
+// بنجيب التوكن من Environment Variables
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const bot = new TelegramBot(token, { polling: true });
-
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-
-    bot.sendMessage(chatId, "✅ البوت شغال بنجاح!");
+// أمر /start
+bot.start((ctx) => {
+  ctx.reply("البوت اشتغل بنجاح 🔥");
 });
 
-console.log("Bot started...");
+// أي رسالة
+bot.on("text", (ctx) => {
+  ctx.reply("استلمت رسالتك 👌");
+});
+
+// تشغيل البوت
+bot.launch();
+
+console.log("Bot is running...");
