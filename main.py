982c19ae67a9bf4abfaf560a8a4b178198a505c8
vkbkjhhii <khas66258@gmail.com>
2026-03-01 20:22:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=BOT_TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# زرار البوت الآخر
-keyboard = InlineKeyboardMarkup()
-button = InlineKeyboardButton(
-    "🚀 بوت آخر",
-    url="https://t.me/ALMNHRF_2bot?start=b43eb21574c4d1585490bb18860d20d88219386"
-)
-keyboard.add(button)
-
-
-@dp.message_handler(commands=["start"])
+@dp.message_handler(commands=['start'])
 async def start(message: types.Message):
-    await message.answer(
-        "أهلاً بيك 👋\nاضغط الزرار تحت 👇",
-        reply_markup=keyboard
-    )
-
+    await message.reply("البوت شغال بنجاح 🔥")
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
