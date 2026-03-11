e3ae830d181b2b77ba52cfcfd67f437565905064
vkbkjhhii <khas66258@gmail.com>
2026-03-11 23:05:33 +0200

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -2,18 +2,19 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # الكيبورد الرئيسي
 def main_menu():
-    kb = InlineKeyboardMarkup(row_width=2)
+    kb = InlineKeyboardMarkup(row_width=3)
+    # الصف الأول
     kb.add(
         InlineKeyboardButton("ارقام فيك 👽", callback_data="numbers"),
-        InlineKeyboardButton("صيد يوزر ✨", callback_data="vip")
-    )
-    kb.add(
+        InlineKeyboardButton("صيد يوزر ✨", callback_data="vip"),
         InlineKeyboardButton("فحص الروابط 🔎", callback_data="check_link")
     )
+    # الصف الثاني
     kb.add(
         InlineKeyboardButton("Farm GPT 😈", url="https://t.me/ALMNHRF_Toobot"),
         InlineKeyboardButton("المطور 🥷", callback_data="contact_dev")
     )
+    # الصف الثالث
     kb.add(
         InlineKeyboardButton("تحت الإصلاح ", callback_data="xo_game")
     )
