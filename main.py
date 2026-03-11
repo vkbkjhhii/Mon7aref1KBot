9a2a07ed6f2d04ad2f9b77145049b2ee24d397f6
vkbkjhhii <khas66258@gmail.com>
2026-03-12 00:31:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from aiogram import Bot, Dispatcher
 from aiogram.utils import executor
-#from handlers import register_handlers
+from handlers import register_handlers
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 DEV_ID = 7771042305  # حط هنا ID المطور
@@ -14,4 +14,3 @@ register_handlers(dp, DEV_ID)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
-    
