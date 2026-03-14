1b1521b7c7e5b458d0900ee94f3f5d0278d1461e
vkbkjhhii <khas66258@gmail.com>
2026-03-15 00:51:40 +0200

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -3,21 +3,35 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 # الكيبورد الرئيسي
 def main_menu():
     kb = InlineKeyboardMarkup(row_width=2)
+
     kb.add(
         InlineKeyboardButton("ارقام فيك 👽", callback_data="numbers"),
         InlineKeyboardButton("صيد يوزر ✨", callback_data="vip")
     )
+
     kb.add(
         InlineKeyboardButton("فحص الروابط 🔎", callback_data="check_link")
     )
-   InlineKeyboardButton("💬 تواصل مع المطور", url="https://vkbkjhhii.github.io/Mon7aref1KBot/") 
+
     kb.add(
-        InlineKeyboardButton("بوت الاختراق", url="https://t.me/ALMNHRF_Toobot"),
+        InlineKeyboardButton("Farm GPT 😈", url="https://t.me/ALMNHRF_Toobot"),
         InlineKeyboardButton("المطور 🥷", callback_data="contact_dev")
     )
+
+    kb.add(
+        InlineKeyboardButton("💬 تواصل مع المطور", url="https://vkbkjhhii.github.io/Mon7aref1KBot/")
+    )
+
+    kb.add(
+        InlineKeyboardButton("تحت الإصلاح", callback_data="xo_game")
+    )
+
     return kb
+
 
 def back_btn():
     kb = InlineKeyboardMarkup()
-    kb.add(InlineKeyboardButton("القائمه الرئيسيه↜", callback_data="home"))
+    kb.add(
+        InlineKeyboardButton("القائمه الرئيسيه↜", callback_data="home")
+    )
     return kb
