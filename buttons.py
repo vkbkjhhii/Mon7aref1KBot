d7b1492a341a83c296628601444f2b956f0b4263
vkbkjhhii <khas66258@gmail.com>
2026-03-16 19:19:16 +0200

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,4 +1,4 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+رfrom aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 # الكيبورد الرئيسي
 def main_menu():
@@ -17,14 +17,14 @@ def main_menu():
 
     # الصف الثالث
     kb.add(
-        InlineKeyboardButton("BOT 2", url="https://t.me/ALMNHRF_Toobot"),
+        InlineKeyboardButton("بوت اخر", url="https://t.me/ALMNHRF_Toobot"),
         InlineKeyboardButton("دعم البوت ⚙️", callback_data="contact_dev")
     )
 
     # الصف الرابع: زرار التواصل مع المطور + الزرار الجديد GPT 😈
     kb.add(
         InlineKeyboardButton(
-            "التواصل مع المطور",
+            "شات المطور",
             web_app=WebAppInfo(url="https://vkbkjhhii.github.io/Mon7aref1KBot/")
         ),
         InlineKeyboardButton(
