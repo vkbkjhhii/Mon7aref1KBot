1a8f01b87b08b3c5ed0fc24542eb17507a8903eb
vkbkjhhii <khas66258@gmail.com>
2026-03-16 17:41:58 +0200

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -4,21 +4,24 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 def main_menu():
     kb = InlineKeyboardMarkup(row_width=2)
 
+    # الصف الأول من الزرار
     kb.add(
         InlineKeyboardButton("ارقام فيك 👽", callback_data="numbers"),
         InlineKeyboardButton("صيد يوزر ✨", callback_data="vip")
     )
 
+    # الصف الثاني
     kb.add(
         InlineKeyboardButton("فحص الروابط 🔎", callback_data="check_link")
     )
 
+    # الصف الثالث
     kb.add(
         InlineKeyboardButton("BOT 2", url="https://t.me/ALMNHRF_Toobot"),
         InlineKeyboardButton("دعم البوت ⚙️", callback_data="contact_dev")
     )
 
-    # سطر الزرار الجديد جنب زرار التواصل مع المطور
+    # الصف الرابع: زرار التواصل مع المطور + الزرار الجديد GPT 😈
     kb.add(
         InlineKeyboardButton(
             "التواصل مع المطور 💻",
@@ -33,6 +36,7 @@ def main_menu():
     return kb
 
 
+# زر الرجوع للقائمة الرئيسية
 def back_btn():
     kb = InlineKeyboardMarkup()
     kb.add(
