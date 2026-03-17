f3da55c8c2f28d6798ee835134a08d855ff6729b
vkbkjhhii <khas66258@gmail.com>
2026-03-17 11:46:25 +0200

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,42 +1,38 @@
-رfrom aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 # الكيبورد الرئيسي
 def main_menu():
     kb = InlineKeyboardMarkup(row_width=2)
 
-    # الصف الأول
     kb.add(
         InlineKeyboardButton("ارقام فيك 👽", callback_data="numbers"),
         InlineKeyboardButton("صيد يوزر ✨", callback_data="vip")
     )
 
-    # الصف الثاني
     kb.add(
         InlineKeyboardButton("فحص الروابط 🔎", callback_data="check_link")
     )
 
-    # الصف الثالث
     kb.add(
-        InlineKeyboardButton("بوت اخر", url="https://t.me/ALMNHRF_Toobot"),
+        InlineKeyboardButton("BOT 2", url="https://t.me/ALMNHRF_Toobot"),
         InlineKeyboardButton("دعم البوت ⚙️", callback_data="contact_dev")
     )
 
-    # الصف الرابع: زرار التواصل مع المطور + الزرار الجديد GPT 😈
+    # سطر الزرار الجديد جنب زرار التواصل مع المطور
     kb.add(
         InlineKeyboardButton(
-            "شات المطور",
+            "التواصل مع المطور 💻",
             web_app=WebAppInfo(url="https://vkbkjhhii.github.io/Mon7aref1KBot/")
         ),
         InlineKeyboardButton(
-            "Farm GPT",
-            web_app=WebAppInfo(url="https://vkbkjhhii.github.io/Mon7aref1KBot/darkweb.html")
+            "دخول Dark Web 🕵️‍♂️",
+            web_app=WebAppInfo(url="https://vkbkjhhii.github.io/darkweb.html")  # الرابط للصفحة المخيفة
         )
     )
 
     return kb
 
 
-# زر الرجوع للقائمة الرئيسية
 def back_btn():
     kb = InlineKeyboardMarkup()
     kb.add(
