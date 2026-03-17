90bb98a552d1fb8cdcfbfc5d02216a682e80aea0
vkbkjhhii <khas66258@gmail.com>
2026-03-17 21:56:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from aiogram import Bot, Dispatcher
 from aiogram.utils import executor
 from handlers import register_handlers
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DEV_ID = 7771042305  # حط هنا ID المطور
+# التوكن من البيئة (يمكنك وضعه مباشرة بدل os.getenv إذا أحببت)
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # أو ضع "توكن البوت هنا"
+DEV_ID = 7771042305  # رقم حساب المطور
 
+# إنشاء كائن البوت و Dispatcher
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot)
 
 # سجل كل handlers
 register_handlers(dp, DEV_ID)
 
+# تشغيل البوت
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
