7f7e0c8978d3ae24d670c6ddaf4ba44e64aca963
vkbkjhhii <khas66258@gmail.com>
2026-03-17 21:45:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+import telebot
+
+# توكن البوت الخاص بك من @BotFather
 BOT_TOKEN = "8768225608:AAHkYYcW-3wIUykW9UIGMh1qug8XtxIPuwY"
-DEV_ID = 7771042305  # رقم المطور
+
+# رقم المطور لتلقي إشعارات أو صلاحيات خاصة
+DEV_ID = 7771042305
+
+# إنشاء كائن البوت
+bot = telebot.TeleBot(BOT_TOKEN)
