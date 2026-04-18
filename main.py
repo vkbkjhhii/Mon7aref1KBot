f2a9778a731b180e0d3aeeec1a8f31f16556d744
vkbkjhhii <khas66258@gmail.com>
2026-04-18 17:12:48 +0200

    Update command registration and fix comments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
 import asyncio
 
 from config import BOT_TOKEN
@@ -7,9 +8,9 @@ from handlers import start, handle_buttons
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-dp.message.register(start, commands=["start"])
+dp.message.register(start, Command("start"))
 
-# مهم نبعته bot للهاندلر
+# باقي الرسائل
 dp.message.register(lambda msg: handle_buttons(msg, bot))
 
 async def main():
